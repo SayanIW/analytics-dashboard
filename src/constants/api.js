506edefe8af669f5903b1c10a211296389f44a74
SayanIW <sayan.dey@inworkglobal.com>
2026-05-14 15:15:46 +0530

    Update API credentials and version in api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -22,7 +22,7 @@ export const USERS_API = {
 export const CONVERSATIONS_API = {
   BASE_URL: 'https://services.leadconnectorhq.com/conversations/search',
   MESSAGES_URL: 'https://services.leadconnectorhq.com/conversations',
-  LOCATION_ID: 'wJ5RzgK5QWJOkfK65b06',
-  TOKEN: 'pit-80915d73-a3b5-4c75-bc78-34750b1c2537',
-  VERSION: '2021-04-15'
+  LOCATION_ID: '0BL2s1FXz9zqmSGr7yns',
+  TOKEN: 'pit-b9cfd4b2-394a-4dc2-a5fe-08373f52bb96',
+  VERSION: '2021-07-28'
 }
